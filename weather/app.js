83a71b9abbbbe09c3e6c6207156a436b5afaaddd
Rakshit Dabral <96056758+rakshitdabral@users.noreply.github.com>
2022-08-19 19:45:21 +0530

    +

---
+++ weather/app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const https = require('https')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+"/index.html")
+    
+})
+
+app.post('/',function(req,res) {
+    const query=req.body.cityName;
+    const apiKey="8ef0a674eb5df59d317dd49f4d4f4249";
+    const units="metric"
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid=8ef0a674eb5df59d317dd49f4d4f4249&units=metric"
+    https.get(url,function(response){
+        console.log(response.statusCode)
+
+        response.on('data', (chunk) => {
+            JSON.parse(chunk)
+            const weatherData = JSON.parse(chunk)
+            const temp=weatherData.main.temp
+            const weatherDesc=weatherData.weather[0].description
+            const icon=weatherData.weather[0].icon
+            const imageURL="http://openweathermap.org/img/wn/" +icon+"@2x.png";
+            res.write("<p>weather description is "+ weatherDesc +"</p>")
+            res.write("<h1>The temperature in "+query +" is "+ temp+ " degree </h1>")
+            res.write(`<img src="${imageURL}" />`)
+            res.send()
+        })
+    })
+})
+
+
+app.listen(port, function (){
+    console.log("server running on port " + port)
+})

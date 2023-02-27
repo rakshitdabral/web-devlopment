ae6456b98abca13631400d238830e5345c4c366f
Rakshit Dabral <96056758+rakshitdabral@users.noreply.github.com>
2023-02-27 19:37:59 +0530

    react

---
+++ keeper/src/components/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer(){
+    const year = new Date()
+    return <footer>
+        <p>Copyright @{year.getFullYear()}</p>
+    </footer>
+}
+
+export default Footer;
